47a6e42b2e5780b73d5673ff84602aee63c5e46c
Chander Raj Singh <chanderrajsingh@gmail.com>
2016-11-10 12:04:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.send('Article one has requested and will be served.');
+   // res.send('Article one has requested and will be served.');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two',function(req,res){
