1a988617edc9e8489886dd2eb02fc5b4f7db5cd6
Chander Raj Singh <chanderrajsingh@gmail.com>
2016-11-20 00:35:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var config = {
     database: 'chanderrajsingh',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB_PASSWORD
+    password: 12345678
 };
 
 var app = express();
